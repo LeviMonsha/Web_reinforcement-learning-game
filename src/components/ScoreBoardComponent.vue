25fc3b3ebad609cf3a58fb13448381549f8ef011
LeviMonsha <lev.coposow@yandex.ru>
2024-11-16 13:56:15 +0300

    Upload site files

---
+++ src/components/ScoreBoardComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="score-board">
+      <h4 class="text-lg">Текущий счет: {{ score }}</h4>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['score'],
+  };
+  </script>
+  
+  <style scoped>
+  .score-board {
+    margin-top: 20px;
+  }
+  </style>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-center">
+      <h2 class="text-2xl font-semibold mb-4">Добро пожаловать в игру!</h2>
+      <GameComponent />
+    </div>
+  </template>
+  
+  <script>
+  import GameComponent from '../components/GameComponent.vue';
+  
+  export default {
+    components: {
+      GameComponent,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
